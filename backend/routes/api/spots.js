6da7ad8e4ae1626320ed83e1bc8bb1d8c01bbb79
Nathan Baldwin <nathanrobertbaldwin@gmail.com>
2023-06-12 22:36:54 -0500

    Still messing with dialect issues.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -16,7 +16,7 @@ router.get("/current", async (req, res, next) => {
       { model: Review, attributes: [] },
       { model: SpotImage, attributes: ["url"], where: { preview: true } },
     ],
-    group: ["Spot.id", "Review.id", "SpotImage.id"],
+    group: ["Spot.id", "Reviews.id", "SpotImages.id"],
   });
 
   spots = spots.map((spot) => (spot = spot.toJSON()));
