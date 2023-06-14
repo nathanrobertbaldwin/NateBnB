1751ad2f496494afcb70f77f339ba4be0fa08e6b
Nathan Baldwin <nathanrobertbaldwin@gmail.com>
2023-06-14 09:48:37 -0500

    Starting work on Delete an image for a spot.

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,18 @@
+// ================ IMPORTS ================ //
+
+const router = require("express").Router();
+const sequelize = require("sequelize");
+const {
+  Spot,
+  Review,
+  SpotImage,
+  User,
+  ReviewImage,
+} = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+// ================ MIDDLEWARE ================ //
+
+
