b9dda61bec2b5a83e62186043d21c997c67be065
Nathan Baldwin <nathanrobertbaldwin@gmail.com>
2023-06-30 15:28:19 -0500

    Rechecked Modals.

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,19 +1,17 @@
 // frontend/src/components/Navigation/index.js
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton";
-import "./Navigation.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
-function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul>
       <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
+        <NavLink exact to="/">Home</NavLink>
       </li>
       {isLoaded && (
         <li>
