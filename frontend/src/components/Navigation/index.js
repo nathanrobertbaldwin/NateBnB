c419c11cd3bd99624c07a238692d27404dec4515
Nathan Baldwin <nathanrobertbaldwin@gmail.com>
2023-06-30 18:04:01 -0500

    Can't figure this out. Will return later.

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,24 +1,31 @@
 // frontend/src/components/Navigation/index.js
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
+    <div id="nav_container">
+      <div id="logo_container">
+        <img id="logo" />
+      </div>
+      <ul id="navigation">
+        <li class="nav_links">
+          <NavLink exact to="/">
+            <button class="nav_button">Home</button>
+          </NavLink>
         </li>
-      )}
-    </ul>
+        {isLoaded && (
+          <li class="nav_links">
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
+      </ul>
+    </div>
   );
 }
 
