397da844ab69e67dd70ba2d60c08d27d6e6ff46b
nathanrobertbaldwin <113254349+nathanrobertbaldwin@users.noreply.github.com>
2023-07-05 13:09:44 -0500

    Committing after rebasing.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -766,6 +766,7 @@ router.put(
 // -------------------------- Delete a Booking ------------------------- //
 
 router.delete("/:spotId", requireAuth, async (req, res, next) => {
+  console.log("Getting here.");
   const spotId = parseInt(req.params.spotId);
   const spot = await Spot.findByPk(spotId);
 
