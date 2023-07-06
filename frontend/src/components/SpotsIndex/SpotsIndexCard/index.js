c71437dc039e0e870ef117978b726844115a02e9
nathanrobertbaldwin <113254349+nathanrobertbaldwin@users.noreply.github.com>
2023-07-06 17:28:18 -0500

    Done with styling on SpotIndex

--- frontend/src/components/SpotsIndex/SpotsIndexCard/index.js
+++ frontend/src/components/SpotsIndex/SpotsIndexCard/index.js
@@ -1,23 +1,30 @@
 // ============================== IMPORTS ============================== //
 
-import "./SpotsIndexCard.css";
 import { Link } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import "./SpotsIndexCard.css";
 
 // ============================= EXPORTS =============================== //
 
 export function SpotsIndexCard({ spot }) {
-  const rating = spot.avgStarRating ? spot.avgStarRating : "no reviews";
   return (
     <div className="spot_card">
       <Link to={`/spots/${spot.id}`}>
-        <div className="spot_preview_image">
-          <img className="spot_card_image" src={spot.previewImage} />
+        <div className="spot_card_image_container">
+          <img
+            className="spot_card_image"
+            src={spot.previewImage}
+            title={spot.name}
+          />
         </div>
-        <div className="">
-          <div>{`${spot.city}, ${spot.state}`}</div>
-          <div>{`Stars: ${rating}`}</div>
+        <div className="city_state_stars">
+          <h3>{`${spot.city}, ${spot.state}`}</h3>{" "}
+          <h3>
+            <FaStar className="review_stars" />
+            {spot.avgStarRating ? spot.avgStarRating.toFixed(1) : "New!"}
+          </h3>
         </div>
-        <div>{`$${spot.price} / night`}</div>
+        <p>{`$${spot.price} per night`}</p>
       </Link>
     </div>
   );
